1a3d9c6c6ef7f0295ea96f09c65e2c4e173de51b
Ian O'Sullivan <ianosullivan@users.noreply.github.com>
2014-05-26 17:11:14 +0100

    Update main.js

--- example_site/js/main.js
+++ example_site/js/main.js
@@ -1,12 +1,5 @@
 $(function(){
 	
-	// We don't want to include the header.cfm and footer.cfm files in AJAX requests so globally add this param to all AJAX requests
-	$.ajaxSetup({
-	    data: {
-	        nolayout: true
-	    }
-	});
-	
 	//Check for window hash message
 	if(window.location.hash != ''){
 		//Extract and decode the message
